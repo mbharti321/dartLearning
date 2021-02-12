ac042fcf3d2b1661cecbfee327db51e06ede50fd
Manish Bharti <mbharti321@gmail.com>
2021-02-12 22:27:14 +0530

    Create final_const.dart

---
+++ final_const.dart
@@ -0,0 +1,11 @@
+void main() {
+    final name = "Manish";
+  const PI = 3.14;
+  
+  print("$name's land area = ${PI * 2 * 2}");
+}
+
+class Circle{
+  final num = 10;
+  static const num2  =20;
+}
