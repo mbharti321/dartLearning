85abc2b51cff151cab1630531d94580bd0284077
Manish Bharti <mbharti321@gmail.com>
2021-02-12 23:02:22 +0530

    Create conditional_statement.dart

---
+++ conditional_statement.dart
@@ -0,0 +1,17 @@
+void main(){
+  
+//   conditional statement
+  var num = 10;
+  var hello = num>10 ? num : num + 2;
+  print(hello);
+  
+  String name = "Manish";
+  String nameString = name ?? "Guest user";
+  print(nameString);
+  
+  String name2 = null;
+  String nameString2 = name2 ?? "Guest user";
+  print(nameString2);
+  
+//   switch can only have int or string variable
+}
