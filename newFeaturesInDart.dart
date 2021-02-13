17923cebc2d332bac676d23b2f252b39328e3ecc
Manish Bharti <mbharti321@gmail.com>
2021-02-13 12:27:47 +0530

    Create newFeaturesInDart.dart

--- newFeaturesInDart.dart
+++ newFeaturesInDart.dart
@@ -1,3 +1,11 @@
+var str1 = "this is Manish Bharti." +
+             "I am from bangalore currently pursuing "
+             " Master of Computer Applicaion.";
+  print("my anem is $str1");
+
+
+
+
 //using fat-arrow (=>) for method shorthand syntax
 void hello() =>  print("Manish");
 
