1f53cebdd05586afb501c7e3c6347b982327281f
Manish Bharti <mbharti321@gmail.com>
2021-02-13 12:18:10 +0530

    Update conditional_statement.dart

--- conditional_statement.dart
+++ conditional_statement.dart
@@ -14,4 +14,20 @@ void main(){
   print(nameString2);
   
 //   switch can only have int or string variable
+}void main(){
+  
+//   conditional statement
+  var num = 10;
+  var hello = num>10 ? num : num + 2;
+  print(hello);
+  
+  String name = "Manish";
+  String nameString = name ?? "Guest user";
+  print(nameString);
+  
+  String name2 = null;
+  String nameString2 = name2 ?? "Guest user";
+  print(nameString2);
+  
+//   switch can only have int or string variable
 }
